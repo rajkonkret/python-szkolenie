bdafabd755dd37bfe1c4514a6afe50ec6db67ce3
rajkonkret <rajkonkret660@gmail.com>
2020-04-20 23:29:50 +0200

    fix bugs in whetjher url

--- wheatehre.py
+++ wheatehre.py
@@ -1,5 +1,5 @@
 import requests
-page = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid=99a24a78addf4a2c41947189fcff67f7&&lang=p&format=jsonl')
+page = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid=99a24a78addf4a2c41947189fcff67f7&&lang=pl&format=json')
 json = page.json()
 temp = json['main']['temp']
 print(round(temp-273.5,2))
