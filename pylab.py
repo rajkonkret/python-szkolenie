abcf78acf2d97d3d50d6e8ed45e219dd2885fc8f
rajkonkret <rajkonkret660@gmail.com>
2020-04-20 22:40:49 +0200

    pyQt5 exercise

---
+++ pylab.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+a = 1
+b = 2
+x = range(-10, 11) # lista argumentów x
+y = [] # lista wartości
+for i in x:
+    y.append(a * i + b)
+plt.plot(x, y)
+plt.title('Wykres f(x) = a*x - b')
+plt.grid(True)
+plt.show()
