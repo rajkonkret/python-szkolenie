b9cc7ac1561b62135ea01f7eae2ad63c7f2600ad
rajkonkret <rajkonkret660@gmail.com>
2020-04-20 23:33:21 +0200

    kelwin -273.15

--- wheatehre.py
+++ wheatehre.py
@@ -2,6 +2,6 @@ import requests
 page = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid=99a24a78addf4a2c41947189fcff67f7&&lang=pl&format=json')
 json = page.json()
 temp = json['main']['temp']
-print(round(temp-273.5,2))
+print(round(temp-273.15,2))
 print(json['name'])
 print(json['sys']['country'])
