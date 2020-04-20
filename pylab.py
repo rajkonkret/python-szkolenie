3345bd188a2ed2938eb916bae423ba3b85d103f8
rajkonkret <rajkonkret660@gmail.com>
2020-04-20 22:57:51 +0200

    2x+1

--- pylab.py
+++ pylab.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-a = 1
-b = 2
-x = range(-100, 11) # lista argumentów x
+a = 2
+b = 1
+x = range(-10, 11) # lista argumentów x
 y = [] # lista wartości
 for i in x:
     y.append(a * i + b)
 plt.plot(x, y)
-plt.title('Wykres f(x) = a*x - b')
-plt.grid(False)
+plt.title('Wykres f(x) = a*x + b')
+plt.grid(True)
 plt.show()
