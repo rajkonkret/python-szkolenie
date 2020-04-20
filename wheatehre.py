35d60b4079f5a950443736a474587a7d5c415389
rajkonkret <rajkonkret660@gmail.com>
2020-04-20 22:18:48 +0200

    wheather some change to parse json

---
+++ wheatehre.py
@@ -0,0 +1,7 @@
+import requests
+page = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid=99a24a78addf4a2c41947189fcff67f7&&lang=p&format=jsonl')
+json = page.json()
+temp = json['main']['temp']
+print(json)
+print(json['name'])
+print(json['sys']['country'])
