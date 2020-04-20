7ad72db65bc27b60a03fcaf9de70a94cb5ef6f14
rajkonkret <rajkonkret660@gmail.com>
2020-04-20 23:27:51 +0200

    wheather in celcius

--- wheatehre.py
+++ wheatehre.py
@@ -2,6 +2,6 @@ import requests
 page = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid=99a24a78addf4a2c41947189fcff67f7&&lang=p&format=jsonl')
 json = page.json()
 temp = json['main']['temp']
-print(json)
+print(round(temp-273.5,2))
 print(json['name'])
 print(json['sys']['country'])
