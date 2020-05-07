38612c799ac44a916b27bcbce3b7f6e836bfcde8
rajkonkret <rajkonkret660@gmail.com>
2020-05-07 18:42:31 +0200

    excersise 4 (if,else)

--- wykres.py
+++ wykres.py
@@ -1,6 +1,28 @@
 import matplotlib.pyplot as p
 x = [1,4,7]
 y = [3,2,10]
-p.plot(x,y)
-p.show()
+print('Test')
+a = int( input('Podaj liczbę 1\n'))
+b = int( input('Podaj liczbę 2\n'))
+c = int( input('Podaj liczbę 3\n'))
+
+if a>b:
+    max = a # a jest kandydatem na maxa
+    print(a)
+else:
+    max = b # b jest kandydatem na maxa
+    print(b)
+if c>max:
+    max = c
+print('max = {}'.format(max))
+word = input('Podaj wyraz czy jest palindromem\n')
+reversed_string = word[::-1]
+if word = reversed_string:
+    print('Ten wyraz jest palindromem')
+else:
+    print('Ten wyraz nie jest palindromem')
+print(reversed_string)
+
+#p.plot(x,y)
+#p.show()
 
