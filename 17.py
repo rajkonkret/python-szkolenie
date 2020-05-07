31af380574fcde67ff2701c189c3ea75b6daa57c
rajkonkret <rajkonkret660@gmail.com>
2020-05-07 20:44:17 +0200

    turtle with square

--- 17.py
+++ 17.py
@@ -1,6 +1,17 @@
 import turtle
 win = turtle.Screen()
 sqare = turtle.Turtle() # zmienna na której możemy rysować
-sqare.forward(100)
+sqare.hideturtle()
+sqare.back(50)
+for i in range(0,4):
+    sqare.forward(100)
+    sqare.left(90)
+
+sqare.left(45)
+for i in range(0,4):
+    sqare.left(90)
+    sqare.forward(100)
+    
+
 win.mainloop()
 
