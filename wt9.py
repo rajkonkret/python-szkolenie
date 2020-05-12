e579cb6d9018444ece96a6ed3b3e6a1e28703e41
rajkonkret <rajkonkret660@gmail.com>
2020-05-12 19:58:36 +0200

    chage randint ex 8

--- wt9.py
+++ wt9.py
@@ -1,6 +1,6 @@
 import random as r
 available_answer = ("tak","nie")
-answer_index = int(r.randint(0,1))
+answer_index = r.randint(0,1)
 print(available_answer[answer_index])
 your_answer = input('Podaj odpowiedź\n')
 
