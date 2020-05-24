e91e9c8828e97951b38ba87ef9fa5977713e6aa0
rajkonkret <rajkonkret660@gmail.com>
2020-05-24 23:31:19 +0200

    ex 8 High Scool finished

--- pi03_zad07liceum.py
+++ pi03_zad07liceum.py
@@ -6,6 +6,14 @@ class HighScool:
     def __repr__(self):
         return self.name + ' needs ' + str(self.min_points) + ' point and is ' + str(self.distance) + ' km away.'
 
+def best_scool(scools):
+    for scool in scools:
+        choose = 'brak szkoły'
+        if scool.distance <= 10 and scool.min_points < 180:
+            choose = scool
+    return choose
+       
+
 first_hs = HighScool('lo zamoyuski', 173,15)
 second_hs = HighScool('LO MK',193,7)
 third_hs = HighScool('LO Zoska',122,6)
@@ -18,3 +26,5 @@ scools.append(third_hs)
 for i in scools:
     print(i)
 
+
+print('Wybrana skoła: ' + best_scool(scools).name)
